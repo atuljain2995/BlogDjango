b5bcd8a3027713e7f67b69d616298a4b28ba953a
atul.jain17@vit.edu <whocanican@2995>
2020-04-19 17:48:29 +0530

    changes made to app

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Dblog.apps.DblogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
